302202ad59158baccc82dfc7dbc73a84dc24b2ee
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-07 13:05:54 +0300

    basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { FC, memo } from 'react';
+import { connect } from 'react-redux';
+import { RootState } from './redux/store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+type Props = {};
 
-export default App;
+const App: FC<Props> = ({}) => {
+  return <div>Hello World</div>;
+};
+
+const mapStateToProps = (state: RootState) => ({});
+const actions = {};
+
+export default connect(mapStateToProps, actions)(memo(App));
